52ad5ac09a8d417314ff183bbd6a1caa68a88adc
Nick Segui <nicksegui5@gmail.com>
2025-09-13 10:29:20 -0500

    completed self assessment

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -1,20 +1,25 @@
-import { getCelebrities } from "./database.js"
+import { getCelebrities } from "./database.js";
 
-const celebrities = getCelebrities()
+const celebrities = getCelebrities();
 
 export const Celebrities = () => {
-    let html = "<ol>"
+  let html = "<ol>";
 
-    for (const star of celebrities) {
-        html += `<li 
-                    data-id="${celebrity.id}" 
-                    data-type="celebrity"
-                    data-sport="${celebrty.sport}"
-                    id="star--${celebrity.id}">
-                    ${celebrity.name}
-                </li>`
-    }
+  for (const celebrity of celebrities) {
+    html += `<li data-id="${celebrity.id}"data-type="celebrity"data-sport="${celebrity.sport}"id="star--${celebrity.id}">${celebrity.name}</li>`;
+  }
 
-    html += "</ol>"
-    return html
-}
+  html += "</ol>";
+  return html;
+};
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  if (itemClicked.dataset.type === "celebrity") {
+    const celebName = itemClicked.textContent;
+    const celebSport = itemClicked.dataset.sport;
+
+    window.alert(`${celebName} is a ${celebSport} star`);
+  }
+});
